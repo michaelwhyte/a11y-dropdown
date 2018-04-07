3cd0ce3bcf79d13b25636b93d2bbe3651d8b64e7
Michael Whyte <info@mwhyte.ca>
2018-04-07 14:11:56 -0700

    Hide open menus when other menu is open

--- navigation.js
+++ navigation.js
@@ -12,6 +12,12 @@ const siteNav = $.querySelector('#primary-menu ul');
 const subNav = siteNav.querySelectorAll('.page_item_has_children');
 // Select the Parent Links in the SubNav
 const subNavMainLink = siteNav.querySelectorAll('.page_item_has_children > a:first-child');
+// Select the Parent Links that are currently in focus
+// *** Note: We use "getElementsByClassName" here as this makes the
+// selection live, meaning it is constantly updated when elements
+// have a class added or removed. On the downside we loose handy 
+// helper functions like "forEach"
+const subNavMainLinksInFocus = $.getElementsByClassName('is-focused');
 // Select the SubNav "ul" elements
 const subNavList = siteNav.querySelectorAll('.children');
 // Select the SubNav Links
@@ -24,6 +30,16 @@ const srt = {
 	expand: 'Expand nav',
 	collapse: 'Collapse nav'
 }
+
+const body = $.body;
+const body2 = $.querySelector('body');
+
+console.log(body2);
+
+body.addEventListener('click', function(){
+	//console.log('hello')
+	hideOtherSubNav();
+});
 
 // Current Page
 // Add aria-describedby to current menu item if any.
@@ -67,24 +83,53 @@ const subNavBtn = document.querySelectorAll('.dropdown-toggle');
 // Mousenter event for subNav
 subNav.forEach(function(el){
 	el.addEventListener('mouseenter', function(){
+		el.classList.add('is-hovered');
+		hideOtherSubNav();
 		setBtnState(el, true);
 	});
 });
 
+function hideOtherSubNav(){
+	for(i = 0; i < subNavMainLinksInFocus.length; i++){
+		if(subNavMainLinksInFocus[i].querySelector(':scope .toggled')){
+			subNavMainLinksInFocus[i].querySelector(':scope .toggled').classList.remove('toggled');
+		}
+		btn = subNavMainLinksInFocus[i].querySelector(':scope .dropdown-toggle');
+		btn.textContent = srt.expand;
+		btn.setAttribute( 'aria-expanded', 'false' )							
+		subNavMainLinksInFocus[i].classList.remove('is-focused');		
+	}
+}
+
 // Mouseleave event for subNav
 subNav.forEach(function(el){
 	el.addEventListener('mouseleave', function(){
+		el.classList.remove('is-hovered');
 		setBtnState(el, false);
 	});
 });
 
 function setBtnState(el, isDisabled){
+	//mainLink = el.firstElementChild;
 	btns = getChildren(el, '.dropdown-toggle');
 	btns.forEach(function(btn){
+		if(el.classList.contains('is-focused') && isDisabled){
+			btn.disabled = true;
+			return;
+		}else if(el.classList.contains('is-focused') && !isDisabled){
+			btn.disabled = false;
+			return;
+		}
 		isDisabled ? btn.textContent = srt.collapse : btn.textContent = srt.expand;
+		isDisabled ? btn.setAttribute( 'aria-expanded', 'true' ) : btn.setAttribute( 'aria-expanded', 'false' );
 		btn.disabled = isDisabled;
 	});
 };
+
+// Change the aria-expanded and button text to match current button behavior.
+// let toggleButton = subNav.querySelector( '.dropdown-toggle' );
+// 'false' === toggleButton.getAttribute( 'aria-expanded' ) ? toggleButton.setAttribute( 'aria-expanded', 'true' ) : toggleButton.setAttribute( 'aria-expanded', 'false' );
+// toggleButton.textContent = ( toggleButton.textContent === screenReaderText.expand ? screenReaderText.collapse : screenReaderText.expand );
 
 // Focus Event for SubNavMainLink
 subNavMainLink.forEach(function(el){
@@ -93,10 +138,13 @@ subNavMainLink.forEach(function(el){
 
 function showSubNavOnFocus(e){
 
+	const parentLi = this.parentElement; 
 	const btn = this.nextElementSibling;
 	const subList = btn.nextElementSibling;
 
+	parentLi.classList.add('is-focused');
 	btn.textContent = srt.collapse;
+	btn.setAttribute( 'aria-expanded', 'true' );
 	subList.classList.add('toggled');
 
 }
@@ -116,52 +164,55 @@ subNavBtn.forEach(function(el){
 });
 
 function showAndHideSubNavOnClick(){
+	this.textContent === srt.expand ? this.setAttribute( 'aria-expanded', 'true' ) : this.setAttribute( 'aria-expanded', 'false' );
 	this.textContent === srt.expand ? this.textContent = srt.collapse : this.textContent = srt.expand;
-	console.log(this.nextElementSibling.classList);
 	this.nextElementSibling.classList.toggle('toggled');
 }
 
 // Keydown Event for last link in
 // Sub Nav list
 subNavLastLinks.forEach(function(el){
-	//el.addEventListener('blur', hideSubNav);
 	el.addEventListener('keydown', function(e){
 		hideSubNav(e, this, false);
 	});
 });
 
 function hideSubNav(e, el, shift){
-	//const subList = this.parentElement.parentElement;
-	//const btn = subList.previousElementSibling;
-
-	//console.log(e.shiftKey);
-	//console.log(e.keyCode);
 
 	if(shift){
+		const parentLi = el.parentElement;
 		const btn = el.nextElementSibling;
 		const subList = btn.nextElementSibling;
-		// This snippet based on this Stackoverflow
-		// answer:
-		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+		// This snippet based on this Stackoverflow queston and answer:
+		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab
 		if(e.keyCode === 9 && e.shiftKey === true){
+			parentLi.classList.remove('is-focused');
 			subList.classList.remove('toggled');
 			btn.textContent = srt.expand;
+			btn.setAttribute( 'aria-expanded', 'false' );
 		}
 	}else{
 		const subList = el.parentElement.parentElement;
+		const parentLi = subList.parentElement;
 		const btn = subList.previousElementSibling;
-		// This snippet based on this Stackoverflow
-		// answer:
-		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+		const mainLink = btn.previousElementSibling;
+		// This snippet based on this Stackoverflow question and answer:
+		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab
 		if(e.keyCode === 9 && e.shiftKey === false){
 			subList.classList.remove('toggled');
 			btn.textContent = srt.expand;
+			btn.setAttribute( 'aria-expanded', 'false' );
+			parentLi.classList.remove('is-focused');
 		}	
 	}
 
 }
 
 // Utility Functions
+
+// This code was modified from from this Stackoverflow question and answer to
+// accept a selector filter:
+// https://stackoverflow.com/questions/842336/is-there-a-way-to-select-sibling-nodes
 function getChildren(parentElement, filter){
 	let r = [];
 	n = parentElement.firstElementChild;
