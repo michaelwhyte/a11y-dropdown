3ddf1ed87324d1b3aec7d165bab3459cac87d3ba
Michael Whyte <info@mwhyte.ca>
2018-04-07 18:29:36 -0700

    Create utility functions

--- navigation.js
+++ navigation.js
@@ -6,7 +6,12 @@
  */
 (function($){
 
-// Nav Dom Elements	
+// Body Element
+const body = $.body;
+// Nav Dom Elements
+// Mobile Menu Toggle Button
+const btnMobileMenuToggle = $.querySelector('.menu-toggle');	
+// Primary Nav
 const siteNav = $.querySelector('#primary-menu ul');
 // Select the subNav elements
 const subNav = siteNav.querySelectorAll('.page_item_has_children');
@@ -18,10 +23,6 @@ const subNavMainLink = siteNav.querySelectorAll('.page_item_has_children > a:fir
 // have a class added or removed. On the downside we loose handy 
 // helper functions like "forEach"
 const subNavMainLinksInFocus = $.getElementsByClassName('is-focused');
-// Select the SubNav "ul" elements
-const subNavList = siteNav.querySelectorAll('.children');
-// Select the SubNav Links
-const subNavLinks = siteNav.querySelectorAll('.children > li > a');
 // Select the SubNav Links that are the last link in
 // their list
 const subNavLastLinks = siteNav.querySelectorAll('.children > li:last-child a'); 
@@ -31,52 +32,70 @@ const srt = {
 	collapse: 'Collapse nav'
 }
 
-const body = $.body;
-const body2 = $.querySelector('body');
-
-console.log(body2);
-
-body.addEventListener('click', function(){
-	//console.log('hello')
-	hideOtherSubNav();
+// Hide open sub navs when body is clicked.
+// The click event handler function for  
+// the body is written further down and  
+// used by other elements
+body.addEventListener('click', function(e){
+	// Ignore clicks on the body that originate from
+	// a dropdown-toggle button
+	if(!e.target.classList.contains('dropdown-toggle')){
+		hideOtherSubNav();
+	}
 });
 
 // Current Page
 // Add aria-describedby to current menu item if any.
+currentPage();
+
 function currentPage() {
 	siteNav.querySelector( '.current_page_item, .current_menu_item' )
 		   .firstElementChild
 		   .setAttribute( 'aria-describedby', 'current' );
 }
-currentPage();
 
 // Initialize Navigation
+navSetup();
+
 function navSetup() {
 
-	// Find drop-downs, add a toggle button to them.
-	function createButton( el ) {
-		
-		const buttonText = document.createTextNode( srt.expand );
-		const newButton = document.createElement( 'button' );
-
-		newButton.classList.add( 'dropdown-toggle' );
-		newButton.setAttribute( 'aria-expanded', 'false' );
-		newButton.appendChild( buttonText );
-
-		el.insertBefore( newButton, el.querySelector( 'ul' ) );
+	// Turn on Mobile Menu Toggle Button for mobile
+	btnMobileMenuToggle.classList.remove('hidden');
 	
-	}
+	// Hide siteNavigation using CSS.
+	siteNav.classList.add('hidden');
 
+	// Create the subNav buttons
 	subNav.forEach(function(el){
 		createButton(el);
 	});
 
-};
+	// Create subNav buttons and 
+	// insert them as the first child of "li" 
+	// elements that are drop-downs
+	function createButton( el ) {
 
-navSetup();
+		// Create button
+		const newButton = document.createElement('button');
+		// Add class to button
+		newButton.classList.add( 'dropdown-toggle' );
+		// Set the state of the button
+		setBtnState(newButton, false, srt.expand, false);
+		// Insert the button into the navigation menu
+		el.insertBefore( newButton, el.querySelector( 'ul' ) );
+	
+	}
+
+}
 
 // Grab the newly created SubNav buttons
 const subNavBtn = document.querySelectorAll('.dropdown-toggle');
+
+// Mobile Menu Toggle
+btnMobileMenuToggle.addEventListener('click', function(){
+	this.getAttribute('aria-expanded') === 'false' ? this.setAttribute('aria-expanded', 'true') : this.setAttribute('aria-expanded', 'false');
+	siteNav.classList.toggle('hidden');
+});
 
 // Event Handlers for Sub Nav items
 
@@ -85,98 +104,101 @@ subNav.forEach(function(el){
 	el.addEventListener('mouseenter', function(){
 		el.classList.add('is-hovered');
 		hideOtherSubNav();
-		setBtnState(el, true);
+		setBtnState(el, true, srt.collapse, true);
 	});
 });
-
-function hideOtherSubNav(){
-	for(i = 0; i < subNavMainLinksInFocus.length; i++){
-		if(subNavMainLinksInFocus[i].querySelector(':scope .toggled')){
-			subNavMainLinksInFocus[i].querySelector(':scope .toggled').classList.remove('toggled');
-		}
-		btn = subNavMainLinksInFocus[i].querySelector(':scope .dropdown-toggle');
-		btn.textContent = srt.expand;
-		btn.setAttribute( 'aria-expanded', 'false' )							
-		subNavMainLinksInFocus[i].classList.remove('is-focused');		
-	}
-}
 
 // Mouseleave event for subNav
 subNav.forEach(function(el){
 	el.addEventListener('mouseleave', function(){
 		el.classList.remove('is-hovered');
-		setBtnState(el, false);
+		setBtnState(el, false, srt.expand, false);
 	});
 });
 
-function setBtnState(el, isDisabled){
-	//mainLink = el.firstElementChild;
-	btns = getChildren(el, '.dropdown-toggle');
-	btns.forEach(function(btn){
-		if(el.classList.contains('is-focused') && isDisabled){
-			btn.disabled = true;
-			return;
-		}else if(el.classList.contains('is-focused') && !isDisabled){
-			btn.disabled = false;
-			return;
+// Hide other sub navigation items
+// when another sub navigation is 
+// hovered or when the user clicks 
+// away from  the navigation menu
+function hideOtherSubNav(){
+	const subNavMainLinksInFocusLength = subNavMainLinksInFocus.length;
+	if(subNavMainLinksInFocusLength === 0){
+		return;
+	}
+	for(i = 0; i < subNavMainLinksInFocusLength; i++){
+		const subNavToggled = subNavMainLinksInFocus[0].querySelector(':scope .toggled');
+		if(subNavToggled){
+			subNavToggled.classList.remove('toggled');
 		}
-		isDisabled ? btn.textContent = srt.collapse : btn.textContent = srt.expand;
-		isDisabled ? btn.setAttribute( 'aria-expanded', 'true' ) : btn.setAttribute( 'aria-expanded', 'false' );
-		btn.disabled = isDisabled;
-	});
-};
-
-// Change the aria-expanded and button text to match current button behavior.
-// let toggleButton = subNav.querySelector( '.dropdown-toggle' );
-// 'false' === toggleButton.getAttribute( 'aria-expanded' ) ? toggleButton.setAttribute( 'aria-expanded', 'true' ) : toggleButton.setAttribute( 'aria-expanded', 'false' );
-// toggleButton.textContent = ( toggleButton.textContent === screenReaderText.expand ? screenReaderText.collapse : screenReaderText.expand );
+		btn = subNavMainLinksInFocus[0].querySelector(':scope .dropdown-toggle');
+		setBtnState(btn, false, srt.expand, false);
+		subNavMainLinksInFocus[0].classList.remove('is-focused');	
+	}
+}
 
 // Focus Event for SubNavMainLink
 subNavMainLink.forEach(function(el){
 	el.addEventListener('focus', showSubNavOnFocus);
 });
 
-function showSubNavOnFocus(e){
-
+function showSubNavOnFocus(){
 	const parentLi = this.parentElement; 
 	const btn = this.nextElementSibling;
 	const subList = btn.nextElementSibling;
 
 	parentLi.classList.add('is-focused');
-	btn.textContent = srt.collapse;
-	btn.setAttribute( 'aria-expanded', 'true' );
+	setBtnState(btn, false, srt.collapse, true);
 	subList.classList.add('toggled');
-
 }
-
-// Keydown Event for SubNavMainLink
-// *** This uses the same function as
-// the subNavLastLinks keydown event
-subNavMainLink.forEach(function(el){
-	el.addEventListener('keydown', function(e){
-		hideSubNav(e, this, true);
-	});
-});
 
 // Click Event for SubNavBtn
 subNavBtn.forEach(function(el){
 	el.addEventListener('click', showAndHideSubNavOnClick);
 });
 
+// Shows and hides subNavs on 
+// click of the subNav buttons.
+// Also changes the state of the 
+// subNav buttons
 function showAndHideSubNavOnClick(){
-	this.textContent === srt.expand ? this.setAttribute( 'aria-expanded', 'true' ) : this.setAttribute( 'aria-expanded', 'false' );
-	this.textContent === srt.expand ? this.textContent = srt.collapse : this.textContent = srt.expand;
+	this.textContent === srt.expand ? setBtnState(this, false, srt.collapse, true) : setBtnState(this, false, srt.expand, false);
+	this.parentElement.classList.add('is-focused');
 	this.nextElementSibling.classList.toggle('toggled');
 }
 
-// Keydown Event for last link in
-// Sub Nav list
+// Keydown event for SubNavMainLinks
+subNavMainLink.forEach(function(el){
+	el.addEventListener('keydown', function(e){
+		hideSubNav(e, this, true);
+	});
+});
+
+// Keydown event for last link in
+// subNav list
 subNavLastLinks.forEach(function(el){
 	el.addEventListener('keydown', function(e){
 		hideSubNav(e, this, false);
 	});
 });
 
+/**
+ * Hide subNav
+ * 
+ * Hides the subNav on a keyboard event.
+ * The subNav that is hidden is dependent on
+ * which element triggered the function.
+ * If the last element in a subNav list triggered
+ * this function, and the "shift" key was not 
+ * pressed, then we select the revelant elements to
+ * hide. If the element was a subNavMainLink
+ * element, and the "shift" key was pressed, then
+ * we select the revelant elements to hide
+ *
+ * @param {Object} e => DOM event object
+ * @param {Object} el => HTML DOM element
+ * @param {Boolean} shift => look for shift key press or not
+ * 
+ */
 function hideSubNav(e, el, shift){
 
 	if(shift){
@@ -186,46 +208,61 @@ function hideSubNav(e, el, shift){
 		// This snippet based on this Stackoverflow queston and answer:
 		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab
 		if(e.keyCode === 9 && e.shiftKey === true){
-			parentLi.classList.remove('is-focused');
-			subList.classList.remove('toggled');
-			btn.textContent = srt.expand;
-			btn.setAttribute( 'aria-expanded', 'false' );
+			hideNav(btn, subList, parentLi);
 		}
 	}else{
 		const subList = el.parentElement.parentElement;
 		const parentLi = subList.parentElement;
 		const btn = subList.previousElementSibling;
-		const mainLink = btn.previousElementSibling;
 		// This snippet based on this Stackoverflow question and answer:
 		// https://stackoverflow.com/questions/3044083/what-is-the-key-code-for-shifttab
 		if(e.keyCode === 9 && e.shiftKey === false){
-			subList.classList.remove('toggled');
-			btn.textContent = srt.expand;
-			btn.setAttribute( 'aria-expanded', 'false' );
-			parentLi.classList.remove('is-focused');
+			hideNav(btn, subList, parentLi);
 		}	
+	}
+
+	function hideNav(button, ul, li){
+		
+		setBtnState(button, false, srt.expand, false);
+		ul.classList.remove('toggled');
+		li.classList.remove('is-focused');
+	
 	}
 
 }
 
 // Utility Functions
 
-// This code was modified from from this Stackoverflow question and answer to
-// accept a selector filter:
-// https://stackoverflow.com/questions/842336/is-there-a-way-to-select-sibling-nodes
-function getChildren(parentElement, filter){
-	let r = [];
-	n = parentElement.firstElementChild;
-    for ( ; n; n = n.nextElementSibling ){ 
-		if(filter){
-			if(n.matches(filter)){
-				r.push( n );
+/**
+ * Set Button State
+ *
+ * @param {Object} el => HTML DOM element
+ * @param {Boolean} disabled => should button be disabled
+ * @param {string} text => text to set for the button
+ * @param {Boolean} ariaExpanded => set the aria-expanded attribute 
+ * 
+ */
+function setBtnState(el, disabled, text, ariaExpanded){
+	
+	if(el.nodeName !== 'BUTTON'){
+		btns = el.querySelectorAll(':scope > .dropdown-toggle');
+		btns.forEach(function(btn){
+			if(el.classList.contains('is-focused') && isDisabled){
+				btn.disabled = true;
+				return;
+			}else if(el.classList.contains('is-focused') && !isDisabled){
+				btn.disabled = false;
+				return;
 			}
-		}else{
-			r.push(n);
-		}				  
-	}        
-    return r;
+			disabled ? setBtnState(btn, true, srt.collapse, true) : setBtnState(btn, false, srt.expand, false);
+		});
+		return;
+	}
+
+	el.textContent = text;
+	el.setAttribute('aria-expanded', ariaExpanded);
+	el.disabled = disabled;
+
 }
 
 })(document);
